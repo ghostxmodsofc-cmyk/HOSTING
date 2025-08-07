e5d959b68bd964f3f650ecf1899ca8866b58ba59
JOBAYER AHMED JIBON <mr.jobayar.ahmed.jibon@gmail.com>
2025-08-07 10:02:17 +0600

    Update app.py

--- app.py
+++ app.py
@@ -321,7 +321,7 @@ def keep_alive():
 # --- End Flask Keep Alive ---
 
 # --- Configuration ---
-TOKEN = '7951184759:AAFzmtTLlaTF4ocUdM-l4Qko5TRia2A_53g' # Replace with your actual token
+TOKEN = '8469111330:AAHkO7sjdi1xDzrjjrG1qV5273c_JRrZK_0' # Replace with your actual token
 OWNER_ID = 6585974275 # Replace with your Owner ID
 ADMIN_ID = 6585974275 # Replace with your Admin ID (can be same as Owner)
 YOUR_USERNAME = '@JOBAYAR_AHMED' # Replace with your Telegram username (without the @)
